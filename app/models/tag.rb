141331cd7f35ebdebb145cd1588d8873c4e205c5
RemyHannequin <remy.hannequin@gmail.com>
2013-02-14 01:29:10 +0100

    Add ManyToMany association between Project and Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ActiveRecord::Base
   attr_accessible :label
+
+  has_and_belongs_to_many :projects
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130213213846) do
+ActiveRecord::Schema.define(:version => 20130214001647) do
 
   create_table "projects", :force => true do |t|
     t.string   "title"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(:version => 20130213213846) do
   end
 
   add_index "projects", ["slug"], :name => "index_projects_on_slug"
+
+  create_table "projects_tags", :id => false, :force => true do |t|
+    t.integer "tag_id",     :null => false
+    t.integer "project_id", :null => false
+  end
 
   create_table "tags", :force => true do |t|
     t.string   "label"
