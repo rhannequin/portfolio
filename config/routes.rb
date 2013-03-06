0439b3e1cbdb0a3ed95cd51888c79dcf7dccc4ad
RemyHannequin <remy.hannequin@gmail.com>
2013-03-06 21:56:37 +0100

    Better admin login lookin

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Portfolio::Application.routes.draw do
 
   root :to => 'projects#index'
 
+  # Portfolio
+  match 'portfolio',        :to => 'portfolio#index',    :as => :portfolio
+  match 'portfolio/:id',    :to => 'portfolio#show',     :as => :show_portfolio
+
   resources :tags
   resources :projects
 
   match :admin,               :to => 'admin#index'
-  get '/admin/login',         :to => 'admin#login', :as => :admin_login
+  get '/admin/login',         :to => 'admin#login',        :as => :admin_login
   post '/admin/authenticate', :to => 'admin#authenticate', :as => :admin_authenticate
   namespace :admin do
     resources :tags
