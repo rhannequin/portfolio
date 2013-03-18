595e593c512b5a5794298d8804434b8f7f7b2f56
RemyHannequin <remy.hannequin@gmail.com>
2013-03-19 00:32:02 +0100

    Better looking admin edit project with bootstrap

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -4,4 +4,9 @@ class Admin::ProjectsController < AdminController
     @projects = Project.order('position').all
   end
 
+  def edit
+    @project = Project.find(params[:id])
+    @projectsCount = Project.all.size
+  end
+
 end
