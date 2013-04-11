a0723db76b8236b5e1f9b7102d9dd8ab5caa5710
rhannequin <remy.hannequin@gmail.com>
2013-04-11 12:15:55 +0200

    Start upload

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :content, :description, :position, :priority, :published, :slug, :tag_ids, :title
+  attr_accessible :content, :description, :position, :priority, :published, :slug, :tag_ids, :title, :assets
 
   has_and_belongs_to_many :tags
 
